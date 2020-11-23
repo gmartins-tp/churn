3fa9811a8d4827403ab801579950affb062ade93
gmartins-tp <74909040+gmartins-tp@users.noreply.github.com>
2020-11-23 12:31:34 +0000

    Update __init__.py

--- tchurn/__init__.py
+++ tchurn/__init__.py
@@ -4,9 +4,9 @@ from .version import __version__
 from .fitters import BaseFitter
 from .fitters.beta_geo_fitter import BetaGeoFitter
 from .fitters.beta_geo_beta_binom_fitter import BetaGeoBetaBinomFitter
-from .fitters.modified_beta_geo_fitter import ModifiedBetaGeoFitter
+from .fitters.modified_beta_geo_fitter import MBGFitter
 from .fitters.pareto_nbd_fitter import ParetoNBDFitter
-from .fitters.gamma_gamma_fitter import GammaGammaFitter
+from .fitters.gamma_gamma_fitter import GGFitter
 
 __all__ = (
     "__version__",
